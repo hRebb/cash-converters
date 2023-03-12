9a0858e94357f533208fc1ff36061d03629e155c
hamzaR <hamza.rebb@gmail.com>
2023-03-12 16:26:54 +0100

    start to dev the app

---
+++ app/app.py
@@ -0,0 +1,43 @@
+from PySide2 import QtWidgets
+import currency_converter
+
+class App(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+        self.c = currency_converter.CurrencyConverter()
+        self.setWindowTitle("Cash Converters")
+        self.setup_ui()
+        self.set_default_values()
+
+    def setup_ui(self):
+        self.layout = QtWidgets.QHBoxLayout(self)
+        self.cbb_devisesFrom = QtWidgets.QComboBox()
+        self.spn_amount = QtWidgets.QSpinBox()
+        self.cbb_devisesTo = QtWidgets.QComboBox()
+        self.spn_amountConverted = QtWidgets.QSpinBox()
+        self.btn_invert = QtWidgets.QPushButton("Invert Devise")
+
+        self.layout.addWidget(self.cbb_devisesFrom)
+        self.layout.addWidget(self.spn_amount)
+        self.layout.addWidget(self.cbb_devisesTo)
+        self.layout.addWidget(self.spn_amountConverted)
+        self.layout.addWidget(self.btn_invert)
+    
+    def set_default_values(self):
+        self.cbb_devisesFrom.addItems(sorted(list(self.c.currencies)))
+        self.cbb_devisesTo.addItems(sorted(list(self.c.currencies)))
+        self.cbb_devisesFrom.setCurrentText("EUR")
+        self.cbb_devisesTo.setCurrentText("EUR")
+
+        self.spn_amount.setRange(1, 1000000)
+        self.spn_amountConverted.setRange(1, 1000000)
+
+        self.spn_amount.setValue(100)
+        self.spn_amountConverted.setValue(100)
+
+app = QtWidgets.QApplication([])
+
+win = App()
+win.show()
+
+app.exec_()
